65ec003c7cc54a1bd15339c33066be0f8f74ca5b
Peter Okorafor <peterdtitan@gmail.com>
2023-07-06 18:04:28 +0300

    Scaffold recipe, food and user

---
+++ spec/views/recipes/index.html.tailwindcss_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe "recipes/index", type: :view do
+  before(:each) do
+    assign(:recipes, [
+      Recipe.create!(
+        name: "Name",
+        preparation_time: 2,
+        cooking_time: 3,
+        description: "MyText",
+        public: false,
+        user_id: 4
+      ),
+      Recipe.create!(
+        name: "Name",
+        preparation_time: 2,
+        cooking_time: 3,
+        description: "MyText",
+        public: false,
+        user_id: 4
+      )
+    ])
+  end
+
+  it "renders a list of recipes" do
+    render
+    cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
+    assert_select cell_selector, text: Regexp.new("Name".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new(2.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new(3.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new("MyText".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new(false.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new(4.to_s), count: 2
+  end
+end
